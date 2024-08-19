cebc775f86a31a7d5d0dcc38d055c6123944afe9
Amr <amrabdelfattah321@gmail.com>
2024-08-19 23:33:22 +0300

    All

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,9 @@
  * prints msg to console
  * @param {string} message - msg to display
  */
-const displayMessage = (\message) => {
-    console.log(message);
-}
+
+const displayMessage = (message) => {
+  console.log(message);
+};
 
 module.exports = displayMessage;
