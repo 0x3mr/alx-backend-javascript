0035162871939b2b3f386536164f2d65d177b0fb
Amr <amrabdelfattah321@gmail.com>
2024-08-20 10:38:56 +0300

    2nd fix

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -16,23 +16,51 @@ const countStudents = (path) => {
             const line = fileContent.split('\n');
 
             for (let i = 0; i < line.length; i++) {
-                if (line[i].length > 0) {
+                if (line[i].length > 0 && line[i].split(',')[0] != "firstname") {
                     numStudents++;
-                    var name = "";
-                    for (let j = 0; j < line[i].length; j++) {
-                        for (let k = 0; line[i][k] != ','; k++) {
-                            if (line[i][k] == ',') {
-                                break;
-                            }
-                            name += line[i][k];
-                        }
-                        
-                        if (name != "") { names.push(name); }
-
-                    }
+                    var name = line[i].split(',')[0];
+                    var major = line[i].split(',')[3];
+                    const dict = `{name: ${name}, major: ${major}}`;
+                    names.push(dict);
                 }
             }
+            var MajorNum = 0;
+            var cName, cMajor;
+            var cNames, cMajors = 0;
+            var cMajorsNames = [];
+            already = [];
+            output = [];
+
+            already1 = [];
+            var cMajor1;
+            for (let i = 0; i < names.length; i++) {
+                cMajor1 = names[i].split(',')[1].slice(8, -1);
+                if (!already.includes(cMajor1)) {
+                    already1.push(cMajor1);
+                    MajorNum++;
+                }
+            }
+            
+            for (let i = 0; i < names.length; i++) {
+                cName = names[i].slice(7, -1).split(',')[0];
+                cMajor = names[i].split(',')[1].slice(8, -1);
+                cMajors = 0;
+                cMajorsNames = [];
+
+                if (!already.includes(cMajor)) {
+                    already.push(cMajor);
+                } else {
+                    cMajors++;
+                    cMajorsNames.push(cName);
+                }
+                const dict = `{major: ${cMajor}, num: ${cMajors}, people: ${cMajorsNames}}`;
+                output.push(dict);
+            }
             console.log(`Number of students: ${numStudents}`);
+            console.log(output);
+
+            // console.log(cMajors);
+            // console.log('Number of students in CS')
         }
     });
   };
