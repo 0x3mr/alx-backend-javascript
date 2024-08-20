833da7f3ed23e753cc84b807a1f24d62298f7130
Amr <amrabdelfattah321@gmail.com>
2024-08-20 15:28:36 +0300

    This should work

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,68 +1,43 @@
-/**
- * prints msg to console
- * @param {string} message - msg to display
- */
+const fs = require('fs');
 
-const countStudents = (path) => {
-    const fs = require('fs');
-    fs.access(path, fs.constants.F_OK, (err) => {
-        if (err) {
-            throw new Error('Cannot load the database');
-        } else {
-            let numStudents = 0;
-            let names = [];
-            
-            const fileContent = fs.readFileSync(path, 'utf8');
-            const line = fileContent.split('\n');
+const countStudents = (dataPath) => {
+  try {
+    const stats = fs.statSync(dataPath);
+    if (!stats.isFile()) {
+      throw new Error('Cannot load the database');
+    }
+    const fileLines = fs
+      .readFileSync(dataPath, 'utf-8')
+      .toString('utf-8')
+      .trim()
+      .split('\n');
+    const studentGroups = {};
+    const dbFieldNames = fileLines[0].split(',');
+    const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-            for (let i = 0; i < line.length; i++) {
-                if (line[i].length > 0 && line[i].split(',')[0] != "firstname") {
-                    numStudents++;
-                    var name = line[i].split(',')[0];
-                    var major = line[i].split(',')[3];
-                    const dict = `{name: ${name}, major: ${major}}`;
-                    names.push(dict);
-                }
-            }
-            var MajorNum = 0;
-            var cName, cMajor;
-            var cNames, cMajors = 0;
-            var cMajorsNames = [];
-            already = [];
-            output = [];
+    for (const line of fileLines.slice(1)) {
+      const studentRecord = line.split(',');
+      const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+      const field = studentRecord[studentRecord.length - 1];
+      if (!Object.keys(studentGroups).includes(field)) {
+        studentGroups[field] = [];
+      }
+      const studentEntries = studentPropNames
+        .map((propName, idx) => [propName, studentPropValues[idx]]);
+      studentGroups[field].push(Object.fromEntries(studentEntries));
+    }
 
-            already1 = [];
-            var cMajor1;
-            for (let i = 0; i < names.length; i++) {
-                cMajor1 = names[i].split(',')[1].slice(8, -1);
-                if (!already.includes(cMajor1)) {
-                    already1.push(cMajor1);
-                    MajorNum++;
-                }
-            }
-            
-            for (let i = 0; i < names.length; i++) {
-                cName = names[i].slice(7, -1).split(',')[0];
-                cMajor = names[i].split(',')[1].slice(8, -1);
-                cMajors = 0;
-                cMajorsNames = [];
+    const totalStudents = Object
+      .values(studentGroups)
+      .reduce((pre, cur) => (pre || []).length + cur.length);
+    console.log(`Number of students: ${totalStudents}`);
+    for (const [field, group] of Object.entries(studentGroups)) {
+      const studentNames = group.map((student) => student.firstname).join(', ');
+      console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
 
-                if (!already.includes(cMajor)) {
-                    already.push(cMajor);
-                } else {
-                    cMajors++;
-                    cMajorsNames.push(cName);
-                }
-                const dict = `{major: ${cMajor}, num: ${cMajors}, people: ${cMajorsNames}}`;
-                output.push(dict);
-            }
-            console.log(`Number of students: ${numStudents}`);
-            console.log(output);
-
-            // console.log(cMajors);
-            // console.log('Number of students in CS')
-        }
-    });
-  };
-
-  module.exports = countStudents;
+module.exports = countStudents;
